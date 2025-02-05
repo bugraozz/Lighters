49ae00d135aad766d958e4e4da2f8202f6a52ccd
Buğra <bugraz725@gmail.com>
2025-02-05 03:43:37 +0300

    10

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -6,7 +6,7 @@ import path from 'path';
 export const config = {
   api: {
     bodyParser: false,
-    sizeLimit: '15mb', // Increase the size limit to 10MB
+    sizeLimit: '15mb', // Increase the size limit to 15MB
   },
 };
 
@@ -14,7 +14,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     const uploadDir = path.join(process.cwd(), 'public/uploads');
 
-    
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
     }
@@ -23,7 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       multiples: true,
       uploadDir: uploadDir,
       keepExtensions: true,
-      maxFileSize: 10 * 1024 * 1024, 
+      maxFileSize: 15 * 1024 * 1024, // 15MB
     });
 
     form.parse(req, (err, fields, files) => {
